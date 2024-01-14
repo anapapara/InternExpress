35d5e77d99bb75df6fbc7007ff64b681304f89ec
Maria Man <manmarilu814@yahoo.com>
2024-01-14 16:18:32 +0200

    THE END...FOR GOOD

--- src/main/java/com/example/internexpress/Main.java
+++ src/main/java/com/example/internexpress/Main.java
@@ -24,7 +24,7 @@ import java.util.Properties;
 public class Main extends Application {
 
     private static Stage stage;
-    private String dbPath = "C:\\Users\\skump\\internExpress\\src\\main\\resources\\bd.properties";
+    private String dbPath = "C:\\Users\\manma\\MasterIS\\Ingineria cerintelor\\InternExpress\\src\\main\\resources\\bd.properties";
 
     public static void main(String[] args) {
         launch(args);
